796a369196e67f9e7739e6b380cfc2fc5ecc8958
Ian144 <ian144@hotmail.com>
2019-05-08 14:59:23 +0100

    TryGet+Set, serverGC

--- Program.cs
+++ Program.cs
@@ -1,114 +1,116 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Jobs;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
-// ReSharper disable ArrangeTypeMemberModifiers
 
+// ReSharper disable ArrangeTypeMemberModifiers
 // ReSharper disable ParameterTypeCanBeEnumerable.Local
 // ReSharper disable UnusedVariable
 
 namespace RoslynImmutableHashMapConsoleApp
 {
     [MemoryDiagnoser]
-    [RyuJitX64Job]
+    //[RyuJitX64Job]
     public class Benchmarks
     {
-        private readonly (string, int n)[] _items = Enumerable.Range(0, 1000).Select(n => (n.ToString(), n)).ToArray();
-        private readonly ImmutableDictionary<string, int> _sciDict;
-        private readonly ImmutableHashMap<string, int> _roslynMap;
+        private readonly Random _rnd;
+        private (string, int n)[] _items;
+        private (string, int n)[] _randItems;
+        private ImmutableDictionary<string, int> _sciDict;
+        private ImmutableHashMap<string, int> _roslynMap;
+        private List<KeyValuePair<string, int>> _kvps;
 
         public Benchmarks()
         {
-            var kvps = _items.Select(t2 => new KeyValuePair<string, int>(t2.Item1, t2.Item2)).ToList();
-            _sciDict = System.Collections.Immutable.ImmutableDictionary.Create<string, int>().AddRange(kvps);
-            _roslynMap = ImmutableHashMap<string,int>.Empty.AddRange(kvps);
+            _rnd = new Random();
+            //_items = Enumerable.Range(0, 10000).Select(n => (n.ToString(), n)).ToArray();
+            //_randItems = _items.OrderBy(_ => _rnd.Next()).ToArray();
+            //_kvps = _items.Select(t2 => new KeyValuePair<string, int>(t2.Item1, t2.Item2)).ToList();
+            //_sciDict = ImmutableDictionary.Create<string, int>().AddRange(_kvps);
+            //_roslynMap = ImmutableHashMap<string, int>.Empty.AddRange(_kvps);
         }
 
-        [Benchmark]
-        public void SysColImmReadItems()
+        [GlobalSetup]
+        public void GlobalSetup()
         {
-            foreach (var (key, _) in _items)
-            {
-                int vvalue = _sciDict[key];
-            }
+            _items = Enumerable.Range(0, 1000000).Select(n => (n.ToString(), n)).ToArray();
+            _randItems = _items.OrderBy(_ => _rnd.Next()).ToArray();
+            _kvps = _items.Select(t2 => new KeyValuePair<string, int>(t2.Item1, t2.Item2)).ToList();
+            _sciDict = ImmutableDictionary.Create<string, int>().AddRange(_kvps);
+            _roslynMap = ImmutableHashMap<string, int>.Empty.AddRange(_kvps);
         }
 
-        [Benchmark]
-        public void RoslynReadItems()
-        {
-            foreach (var (key, n) in _items)
-            {
-                int vvalue = _roslynMap[key];
-            }
-        }
-
-
-
-
-        [Benchmark]
-        public void SysColImmAddItems()
-        {
-            var map = ImmutableDictionary<string, int>.Empty;
-
-            foreach (var (key, n) in _items)
-            {
-                map = map.Add(key, n);
-            }
-        }
-
-        [Benchmark]
-        public void SysColImmAddItemsBuilder()
-        {
-            var bldr = ImmutableDictionary.CreateBuilder<string, int>();
-
-            foreach (var (key, n) in _items)
-            {
-                bldr.Add(key, n);
-            }
-
-            var map = bldr.ToImmutable();
-        }
-
-
-
-        [Benchmark]
-        public void RoslynAddItems()
-        {
-            var map = ImmutableHashMap<string, int>.Empty;
-
-            foreach (var (key, n) in _items)
-            {
-                map = map.Add(key, n);
-            }
-        }
+        //[Setup]
+        //public void Setup()
+        //{
+        //    _sciDict = ImmutableDictionary.Create<string, int>().AddRange(_kvps);
+        //    _roslynMap = ImmutableHashMap<string, int>.Empty.AddRange(_kvps);
+        //}
 
         //[Benchmark]
-        //public void SysColImmGetOrAddItems()
+        //public void SysColImmTryGetValue()
+        //{
+        //    foreach (var (key, _) in _randItems)
+        //    {
+        //        bool ok = _sciDict.TryGetValue(key, out int vvalue);
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void RoslynTryGetValue()
+        //{
+        //    foreach (var (key, n) in _randItems)
+        //    {
+        //        bool ok = _roslynMap.TryGetValue(key, out int vvalue);
+        //    }
+        //}
+
+        [Benchmark]
+        public void SysColImmTryGetSet()
+        {
+            foreach (var (key, _) in _randItems)
+            {
+                bool _ = _sciDict.TryGetValue(key, out int vvalue);
+                _sciDict = _sciDict.SetItem(key, ++vvalue);
+            }
+        }
+
+        [Benchmark]
+        public void RoslynTryGetSet()
+        {
+            foreach (var (key, n) in _randItems)
+            {
+                bool _ = _roslynMap.TryGetValue(key, out int vvalue);
+                _roslynMap = _roslynMap.SetItem(key, ++vvalue);
+            }
+        }
+
+
+        //[Benchmark]
+        //public void SysColImmSetItem()
         //{
         //    var map = ImmutableDictionary<string, int>.Empty;
 
         //    foreach (var (key, n) in _items)
         //    {
-        //        map = map.GetAdd(key, n);
+        //        map = map.SetItem(key, n);
         //    }
         //}
 
-
         //[Benchmark]
-        //public void RoslynGetOrAddItems()
+        //public void RoslynSetItem()
         //{
         //    var map = ImmutableHashMap<string, int>.Empty;
 
         //    foreach (var (key, n) in _items)
         //    {
-        //        map = ImmutableHashMapExtensions.GetOrAdd(map, key, n);
+        //        map = map.SetItem(key, n);
         //    }
         //}
-
     }
 
 
@@ -116,8 +118,8 @@ namespace RoslynImmutableHashMapConsoleApp
     {
         private static void Main()
         {
-            
-            var summary = BenchmarkRunner.Run<Benchmarks>();
+
+            var _ = BenchmarkRunner.Run<Benchmarks>(DefaultConfig.Instance.With(Job.RyuJitX64.WithGcServer(true)));
         }
     }
 }
